81576ece5f6aabb415e75ae494937a27462b48d3
sonlv94 <soncuc182304@gmail.com>
2024-06-27 19:04:02 +0900

    add try -> except

--- Project/utils/dataPath.py
+++ Project/utils/dataPath.py
@@ -5,10 +5,10 @@ class SwitchCase:
 
     def filePath(self):
         if self.name == "country_food_filepath":
-            return r"C:\Users\s-le\Desktop\study\Python\Python-study\Project\data\country_food_overTime_df.csv"
+            return r"C:\Users\s-le\Desktop\study-private\test\Study-Python\Project\data\country_food_overTime_df.csv"
         elif self.name == "vietnam_food_filepath":
-            return r"C:\Users\s-le\Desktop\study\Python\Python-study\Project\data\vietnam_food_overTime_df.csv"
+            return r"C:\Users\s-le\Desktop\study-private\test\Study-Python\Project\data\vietnam_food_filepath.csv"
         elif self.name == "language":
-            return r"C:\Users\s-le\Desktop\study\Python\Python-study\Project\data\language.csv"
+            return r"C:\Users\s-le\Desktop\study-private\test\Study-Python\Project\data\language.csv"
         # else:
         #     return "C:/path/to/daughter_file.csv"
